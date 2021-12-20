f47a056fac47f3a101628583db1e039431421ae8
Nurtugan Nuraly <nurtugan757@gmail.com>
2021-12-21 01:00:36 +0600

    Compose list row

---
+++ iDine/ItemRow.swift
@@ -0,0 +1,28 @@
+//
+//  ItemRow.swift
+//  iDine
+//
+//  Created by Nurtugan Nuraly on 12/21/21.
+//
+
+import SwiftUI
+
+struct ItemRow: View {
+    let item: MenuItem
+    
+    var body: some View {
+        HStack {
+            Image(item.thumbnailImage)
+            VStack(alignment: .leading) {
+                Text(item.name)
+                Text("$\(item.price)")
+            }
+        }
+    }
+}
+
+struct ItemRow_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemRow(item: MenuItem.example)
+    }
+}
