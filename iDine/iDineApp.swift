06de998aec218434285c9fe8e16fe67c51196ae6
Nurtugan Nuraly <nurtugan757@gmail.com>
2021-12-20 21:28:46 +0600

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Nurtugan Nuraly on 12/20/21.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
