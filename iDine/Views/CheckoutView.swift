0861a758696a7dfaace1bb12dc03c38647044dfd
Nurtugan Nuraly <nurtugan757@gmail.com>
2021-12-23 16:49:41 +0600

    Setup alert view

--- iDine/Views/CheckoutView.swift
+++ iDine/Views/CheckoutView.swift
@@ -13,6 +13,7 @@ struct CheckoutView: View {
     @State private var addLoyaltyDetails = false
     @State private var loyaltyNumber = ""
     @State private var tipAmount = 15
+    @State private var showingPaymentAlert = false
     
     private let paymentTypes = ["Cash", "Credit Card", "iDine Points"]
     private let tipAmounts = [10, 15, 20, 25, 0]
@@ -50,12 +51,15 @@ struct CheckoutView: View {
             }
             Section(header: Text("TOTAL: \(totalPrice)")) {
                 Button("Confirm order") {
-                    // todo
+                    showingPaymentAlert.toggle()
                 }
             }
         }
         .navigationTitle("Payment")
         .navigationBarTitleDisplayMode(.inline)
+        .alert(isPresented: $showingPaymentAlert) {
+            Alert(title: Text("Order confirmed"), message: Text("Your total was \(totalPrice) - thank you!"), dismissButton: .default(Text("OK")))
+        }
     }
 }
 
